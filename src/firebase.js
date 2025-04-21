c13854e8c74499af24b1e29b7ba3ee72e9903fd2
Aditya Dixit <dixitaditya619@gmail.com>
2025-04-21 23:05:25 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "MY_API_KEY",
-    authDomain: "MY_AUTH_DOMAIN",
-    projectId: "MY_P_ID",
-    storageBucket: "MY_BUCKET",
-    messagingSenderId: "MY_S_ID",
-    appId: "MY_A_ID",
-    measurementId: "MY_M_ID"
+    apiKey: process.env.MY_API_KEY,
+    authDomain: process.env.MY_AUTH_DOMAIN,
+    projectId: process.env.MY_P_ID,
+    storageBucket: process.env.MY_BUCKET,
+    messagingSenderId: process.env.MY_S_ID,
+    appId: process.env.MY_A_ID,
+    measurementId: process.env.MY_M_ID
 };
 
 const app = initializeApp(firebaseConfig);
